1b401cf78f52d6c4a636c003aa6a76b08f27a0b1
Nikolay Vasilev <coder.servoper@gmail.com>
2025-07-25 17:34:28 +0300

    Remove debug logs

--- app/src/main/java/com/example/mazesolver/list/presentation/viewmodel/MazeListViewModel.kt
+++ app/src/main/java/com/example/mazesolver/list/presentation/viewmodel/MazeListViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.mazesolver.list.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mazesolver.core.domain.util.onError
